6dd85f6c69a37df2b35559f03135eb7cbd8033e0
Kim, Hyeonseo <dodok8@gmail.com>
2026-07-04 01:06:08 +0900

    Add drfed favicon

--- packages/web/src/entry-server.tsx
+++ packages/web/src/entry-server.tsx
@@ -8,7 +8,10 @@ export default createHandler(() => (
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/favicon.ico" sizes="32x32" />
+          <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+          <link rel="apple-touch-icon" href="/favicon-180.png" />
+          <link rel="manifest" href="/manifest.webmanifest" />
           {assets}
         </head>
         <body>
